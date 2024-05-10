276c7122973d610cd2fffc53d66f69f262b82ec5
teresadkla <teresafernandeslbbh@gmail.com>
2024-05-10 01:44:39 +0100

    navbar lol

---
+++ appti/src/componentes/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+function Navbar({ alarme, home, areaPessoal }) {
+    return (
+      <div>
+        <h3>
+          <a href={alarme}>Alarme</a>
+        </h3>
+        <h3>
+          <a href={home}>Home</a>
+        </h3>
+        <h3>
+          <a href={areaPessoal}>Área Pessoal</a>
+        </h3>
+      </div>
+    );
+  }
+     /*
+      nunca posso retornar duas divs 
+      nunxa escrever mais de um componente por ficheiro 
+      */
+  
+  
+
+  export default Navbar;
