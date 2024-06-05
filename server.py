a5d157b1a5471f3848601888baf8c81435dac895
Hirvikallio <carolinahirvikallio@gmail.com>
2024-06-05 19:12:47 +0100

    comentar arduino

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-import serial as s
+# import serial as s
 import schedule
 import time
 import json
@@ -12,12 +12,12 @@ import threading
 # import multiprocessing
 import os
 
-ser = s.Serial(
+'''ser = s.Serial(
     port='COM5',
     baudrate=9600,
     timeout=1
             )
-time.sleep(2)
+time.sleep(2)'''
 
 
 all_meals = []
