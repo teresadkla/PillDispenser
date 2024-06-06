861b9cd36d82437857f385e1882bde76d95266b7
mavandrade4 <mavandrade4@gmail.com>
2024-06-06 17:02:27 +0100

    hours fixed

--- src/pages/AlarmContext.jsx
+++ src/pages/AlarmContext.jsx
@@ -5,6 +5,7 @@ const AlarmContext = createContext();
 
 export const AlarmProvider = ({ children }) => {
   const [currentAlarm, setCurrentAlarm] = useState(null);
+  const [alarmAcknowledged, setAlarmAcknowledged] = useState(false);
 
   useEffect(() => {
     const checkAlarms = async () => {
@@ -13,9 +14,32 @@ export const AlarmProvider = ({ children }) => {
         const now = new Date();
         const currentHours = now.getHours();
         const currentMinutes = now.getMinutes();
+
+        if (alarmAcknowledged) return; // Don't check alarms if already acknowledged
+
+        let cHours;
+        let cMinutes;
+
         const currentMeal = data.meals.find(
-          meal => meal.hours === currentHours && meal.minutes === currentMinutes
+          meal => {
+            if(currentHours < 10){
+              cHours = `0${currentHours}`;
+            }
+            else{
+              cHours = currentHours;
+            }
+            
+            if(currentMinutes < 10){
+                cMinutes = `0${currentMinutes}`;
+            }
+            else{
+                cMinutes = currentMinutes
+            }
+            console.log(`${cHours}:${cMinutes}`)
+            return meal.hours === cHours && meal.minutes === cMinutes
+          }
         );
+
         if (currentMeal) {
           setCurrentAlarm(currentMeal);
         } else {
@@ -26,14 +50,14 @@ export const AlarmProvider = ({ children }) => {
       }
     };
 
-    const intervalId = setInterval(checkAlarms, 60000); // Check every minute
+    const intervalId = setInterval(checkAlarms, 10000); // Check every minute
     checkAlarms(); // Initial check
 
     return () => clearInterval(intervalId); // Cleanup on unmount
-  }, []);
+  }, [alarmAcknowledged]);
 
   return (
-    <AlarmContext.Provider value={{ currentAlarm, setCurrentAlarm }}>
+    <AlarmContext.Provider value={{ currentAlarm, setCurrentAlarm, setAlarmAcknowledged }}>
       {children}
     </AlarmContext.Provider>
   );
